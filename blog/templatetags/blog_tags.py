807c501bba9c216e6b8d9dade6ba1b2b9e6f5fe7
Alireza <alirezasadeghi297@gmail.com>
2025-01-12 18:54:49 +0330

    latest post and categories

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from blog.models import Post
+from blog.models import Category
 from django.utils import timezone
 
 current_time=timezone.now()
@@ -19,3 +20,21 @@ def posts():
 @register.filter
 def snippets(value,args : int =20):
     return value[:args]
+
+
+@register.inclusion_tag('blog/blog_latest_post.html')
+def latest_posts(args : int =2):
+    posts=Post.objects.filter(status=1,published_date__lt=current_time).order_by('published_date')
+
+    return {'latest_posts': posts[:args]}
+
+@register.inclusion_tag('blog/blog_post_cat.html')
+def categories():
+    categories=Category.objects.all()
+    posts=Post.objects.filter(status=1,published_date__lt=current_time)
+    cat_names={}
+    for name in categories:
+        cat_names[name]=posts.filter(category=name).count()
+
+    return {'categories': cat_names}
+    
